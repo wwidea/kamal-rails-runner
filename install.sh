6b7d0c34ffe5344ace7c37c2d9044833378df0cc
Aaron Baldwin <baldwina@brightwayslearning.org>
2025-04-10 12:33:51 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -sSL https://raw.githubusercontent.com/wwidea/kamal-rails-runner/main/kamal-rails-runner.sh -o /usr/local/bin/kamal-rails-runner
+chmod +x /usr/local/bin/kamal-rails-runner
